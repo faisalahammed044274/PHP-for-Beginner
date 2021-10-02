96f4483f18cc90c3ce4c9c70859b360dcb18a0e7
faisalahammed044274 <setu2014@gmail.com>
2021-10-02 21:38:27 +0600

    variable and constants

---
+++ 03.variable and constants/11.PredefinedVariables.php
@@ -0,0 +1,33 @@
+<?php
+
+// 1) Use 1 Predefined Variable
+// 2) Try yourself other Predefined Variables from the url.
+
+//---------------------------
+//   Predefined Variables   \\
+//---------------------------
+
+// Superglobals — Built-in variables that are always available in all scopes
+// $GLOBALS — References all variables available in global scope
+// $_SERVER — Server and execution environment information
+// $_GET — HTTP GET variables
+// $_POST — HTTP POST variables
+// $_FILES — HTTP File Upload variables
+// $_REQUEST — HTTP Request variables
+// $_SESSION — Session variables
+// $_ENV — Environment variables
+// $_COOKIE — HTTP Cookies
+// $php_errormsg — The previous error message
+// $http_response_header — HTTP response headers
+// $argc — The number of arguments passed to script
+// $argv — Array of arguments passed to script
+
+$val = 1/0;
+echo $php_errormsg;
+
+
+
+
+
+
+?>
---
+++ 03.variable and constants/12.DefineVariable-isset.php
@@ -0,0 +1,17 @@
+<?php
+
+// DefineVariable
+
+// Empty Variable
+// $name;
+// echo $name . "<br>";
+
+//Ternary Operator
+echo isset($name) ? "Variable Set" : "Variable is not set" . "<br>";
+
+$name = "Setu, Faisal";
+echo isset($name) ? "Variable Set" : "Variable is not set" . "<br>";
+
+$firstname = "test";
+$name = $firstname;
+echo isset($name) ? "Variable Set" : "Variable is not set" . "<br>";
