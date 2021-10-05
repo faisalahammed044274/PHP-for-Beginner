70aa5996b154f7e2ff7aab29f1d6bae579bfdd2c
faisalahammed044274 <setu2014@gmail.com>
2021-10-05 22:23:04 +0600

    arithmetic operator

--- 04.expressions and operators/01.Operators/01.ArithmeticalOperators1.php
+++ 04.expressions and operators/01.Operators/01.ArithmeticalOperators1.php
@@ -1,8 +1,9 @@
 <?php
 
 //--------Sample 1--------ArithmeticalAddOperators
-// 1) Calculate the Student Total Marks for 3 Subjects
-
+echo "<br>";
+echo "1) Calculate the Student Total Marks for 3 Subjects";
+echo "<br>";
 $maths = 70;
 $science = 30;
 $english = 80;
@@ -10,7 +11,8 @@ $totalMarks = $maths + $science +$english;
 echo "Student Total Marks : $totalMarks for Maths($maths) + Science($science) + English($english)" . "<br>" . PHP_EOL;
 
 //--------Sample 2--------ArithmeticalAddOperators
-// 2) Display the total without a $totalMarks Variable
+echo "<br>";
+echo "2) Display the total without a $totalMarks Variable";
 echo "<br>";
 $maths = 70;
 $science = 30;
@@ -18,42 +20,54 @@ $english = 80;
 echo "Student Total Marks :". ($maths + $science +$english) ."for Maths($maths) + Science($science) + English($english)" . "<br>" . PHP_EOL;
 
 //--------Sample 3--------ArithmeticalSubtractOperators
-// 3) Find the difference between marks achieved and total marks
+echo "<br>";
+echo "3) Find the difference between marks achieved and total marks";
+echo "<br>";
 $totalSubjects = 300;
 $totalDifference = $totalSubjects - $totalMarks;
 echo "Difference between total marks and achived marks :" . $totalDifference . "<br>";
 
 //--------Sample 4--------ArithmeticalMultiplyOperators
-// 4) Calculate the area using length and breath
+echo "<br>";
+echo "4) Calculate the area using length and breath";
+echo "<br>";
 $length = 10;
 $breath = 20;
 $area = $length * $breath;
 echo $area . PHP_EOL;
 
 //--------Sample 5--------ArithmeticalDivideOperators
-// 5) Calculate the Percentage of Overall Scored Marks
+echo "<br>";
+echo "5) Calculate the Percentage of Overall Scored Marks";
+echo "<br>";
 
 $percentage = ($totalMarks / $totalSubjects) * 100;
 echo "Percentage Scored : $percentage%" . PHP_EOL ."<br>";
 
 //--------Sample 6--------ArithmeticalModulusOperators
-// 6) Check if the remainder is 0 or 1
+echo "<br>";
+echo "6) Check if the remainder is 0 or 1";
+echo "<br>";
 
 $a = 100 % 6;
 echo $a . PHP_EOL;
+echo "<br>";
 
 $b = 101 % 5;
 echo $b . PHP_EOL;
+echo "<br>";
 
 $a = 105 % 5;
 echo $a . PHP_EOL;
+echo "<br>";
 
 $b = 106 % 5;
 echo $b . PHP_EOL;
 
 
 //--------Sample 7---------ArithmeticalExponentOperators
-// 7) Use Multiplication Operator || Exponentiation
+echo "<br>";
+echo "7) Use Multiplication Operator || Exponentiation";
 echo "<br>";
 $a = 10;
 $b = 2;
