8ddec056463079de19c3baa6007951f84c02fabd
faisalahammed044274 <setu2014@gmail.com>
2021-10-10 20:30:20 +0600

    Exercise 1

--- 04.expressions and operators/08.ErrorControlOperators.php
+++ 04.expressions and operators/08.ErrorControlOperators.php
@@ -1,9 +1,27 @@
 <?php
 // Raise an Error and Suppress it
-echo "No Errors:" . error_get_last()[error msg] .PHP_EOL;
-$value = 1 / 0;
+
+
+//echo "No Errors:" . error_get_last() . PHP_EOL;
+//$value = 1 / 0;
 //This line is a error and will not proceed further.
 
-echo "Errors: " .error_get_last()['meaggage of error'] . PHP_EOL;
+// echo "Errors:" . error_get_last() . PHP_EOL;
+
+// $php_errormsg — The previous error message
+// Warning
+
+// This feature has been DEPRECATED as of PHP 7.2.0. Relying on this feature is highly discouraged.
+
+// Use error_get_last() instead.
+
+//While $php_errormsg is a global, it is not a superglobal.
+
+//You'll have to qualify it with a global keyword inside a function.
+
+
+
+
+
 
 ?>
---
+++ 04.expressions and operators/1.exercise-OperatorsPrecedence.php
@@ -0,0 +1,41 @@
+// Understand How Operator Precedence Works
+----------------------------------------------------------
+// 1. Add two numbers using Arithmetical Operators.
+// 2. Find Student passed or failed using conditional Operator.
+// 3. Show a number is even or odd using Comparison Operators.
+// 4. Check if user has access and login rights using logical Operators.
+// 5. Assign a integer value and increment by 10 using Assignment Operator.
+
+
+<h3>1. Add two numbers using Arithmetical Operators.</h3>
+
+<?php
+$number1 = 45;
+$number2 = 5;
+
+$addition = ($number1 + $number2);
+
+echo $addition . PHP_EOL . "<br>";
+?>
+
+<h3>2. Find Student passed or failed using conditional Operator.</h3>
+
+<?php
+// $result = (0) ? "Passed" : " Failed";
+// echo "This student is :" . $result . PHP_EOL;
+
+$result = "Passsed";
+echo ($result === "Passed") ? "Student is Passed" : "Student is Failed";
+?>
+
+<h3>3. Show a number is even or odd using Comparison Operators</h3>
+
+<?php
+$number = 10;
+$result = $number%2;
+echo ( $result = 0 ) ? "Even Number" : "Odd Number";
+?>
+
+<h3>4. Check if user has access and login rights using logical Operators</h3>
+
+<h3>5. Assign a integer value and increment by 10 using Assignment Operator</h3>
