1c6ed77ad1ed6de9bc959678bf512c4dafaee21c
faisalahammed044274 <setu2014@gmail.com>
2021-10-11 01:51:52 +0600

    Operator Precedence

---
+++ 04.expressions and operators/09.OperatorsPrecedence.php
@@ -0,0 +1,35 @@
+<?php
+
+//Understand How Operator Precedence Works
+
+/* 
+* Unary - !, ++, --
+* Multiplication - * / %
+* Add/Substract - +, -
+* Relational - <, >, <=, >=
+* Equality - ==, !=
+* Logical AND - &&
+* Logical OR - ||
+* Conditional - ? :
+* Assignment - =, +=, -=, *=, /=, %= 
+*/
+
+$val = 10;
+echo $val++;
+$counter = $val++;
+echo $counter;
+echo $val;
+
+//Guess the output
+$val = 5 * 2 +5;
+echo $value;
+
+//Classic - Assignment has higher precedence over and 
+$result1 = true && false;
+$result2 = true and false; //DONT GET (and false) PART
+
+echo ($result1) ? "true" : "false";
+echo PHP_EOL;
+echo ($result2) ? "true" : "false";
+echo PHP_EOL;
+?>
