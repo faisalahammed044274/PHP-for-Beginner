2dc8e31fb192b1e72923928dac2c25bd75819fb6
faisalahammed044274 <setu2014@gmail.com>
2021-10-12 00:59:57 +0600

    Bitwise Operator

--- 04.expressions and operators/06.BitwiseOperators.php
+++ 04.expressions and operators/06.BitwiseOperators.php
@@ -9,7 +9,22 @@
 ~ = NOT - Works on one operator like ++ and -- Unary Operator
 
 
-//--------------------------------------------------------------------
+//-----------------------------------------------------------------------
+
+// $a & $b ----	    And             ----	Bits that are set in both $a and $b are set.
+
+// $a | $b ----	    Or              ----    (inclusive or) 	Bits that are set in either $a or $b are set.
+
+// $a ^ $b ----	    Xor             ----    (exclusive or) 	Bits that are set in $a or $b but not both are set. 
+
+// ~ $a 	----    Not 	        ----    Bits that are set in $a are not set, and vice versa. 
+
+// $a << $b ----	Shift left 	    ----    Shift the bits of $a $b steps to the left (each step means "multiply by two") 
+
+// $a >> $b ----	Shift right     ----    Shift the bits of $a $b steps to the right (each step means "divide by two") 
+
+//-----------------------------------------------------------------------
+
 
 // How Bits Work
 
@@ -26,21 +41,6 @@
 // // 1 - 0 = 0;
 // */
 
-//-----------------------------------------------------------------------
-
-// $a & $b ----	    And             ----	Bits that are set in both $a and $b are set.
-
-// $a | $b ----	    Or              ----    (inclusive or) 	Bits that are set in either $a or $b are set.
-
-// $a ^ $b ----	    Xor             ----    (exclusive or) 	Bits that are set in $a or $b but not both are set. 
-
-// ~ $a 	----    Not 	        ----    Bits that are set in $a are not set, and vice versa. 
-
-// $a << $b ----	Shift left 	    ----    Shift the bits of $a $b steps to the left (each step means "multiply by two") 
-
-// $a >> $b ----	Shift right     ----    Shift the bits of $a $b steps to the right (each step means "divide by two") 
-
-//-----------------------------------------------------------------------
 $first =  0; //0000
 $second = 1; //0001
 
