8fc9ef4928ae802afcaa47f1a050223769d3a48d
faisalahammed044274 <setu2014@gmail.com>
2021-10-13 01:28:47 +0600

    INTEGER DATATYPE

--- 05.Data Types/02.IntergerDataType.php
+++ 05.Data Types/02.IntergerDataType.php
@@ -18,7 +18,33 @@ Hexadecimal(base 16) and binary (base 2) Values
 
 */
 
+//Define and Print Integer Variable
 
+$marks = 87;
+echo $marks . PHP_EOL;
+echo "<br>";
+//HexaDecimal variables
+$marks = 0xA9C0B95B4; //45567677876
+//convert decimal to hexadecimal then it
+// shows and for hexadecimal we have to use 0x.
+echo $marks . PHP_EOL;
+echo "<br>";
+//String to Integer Addition
+$marks = 10;
+$total = "1000" + $marks;
+echo $total . PHP_EOL;
 
+//Negetive Numbers
+$finalMarks = 5 - 10;
+echo $finalMarks . PHP_EOL;
+
+$finalMarks = -5 + 5;
+echo $finalMarks . PHP_EOL;
+echo "<br>";
+//Windows
+
+echo PHP_INT_SIZE . PHP_EOL;
+
+echo PHP_INT_MAX . PHP_EOL;
 
 ?>
