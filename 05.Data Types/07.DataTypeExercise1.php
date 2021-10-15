de4ea923230fc9a909db9109754c76c1b2bfa8dc
faisalahammed044274 <setu2014@gmail.com>
2021-10-16 02:48:19 +0600

    Data Type

--- 05.Data Types/07.DataTypeExercise1.php
+++ 05.Data Types/07.DataTypeExercise1.php
@@ -1,98 +1,59 @@
-<?php
-//--------------------------
-echo "<center><h1>Integer Data Type </h1></center>" . PHP_EOL;
-//--------------------------
+<!doctype html>
+<html>
 
-echo "<h3>Define and Print Integer Variable :</h3>" . PHP_EOL;
-$a = 23;
-$b = 37;
-$c = $a+$b . PHP_EOL;
-echo $c . PHP_EOL;
-echo "<h3>HexaDecimal variables :</h3>" . PHP_EOL;
-$c = 0x1C1A49AFF; //7543757567 || decimal to hexa decimal
-echo $c . PHP_EOL;
+<head>
+    <title>
+        Data Types
+    </title>
+</head>
 
-echo "<h3>String to Integer Addition :</h3>" . PHP_EOL;
-$number1 = 500;
-$number2 = $number1 + 50;
-echo $number2 . PHP_EOL;
+<body style="background-color:chartreuse">
 
+    <h1>Exercise 1: Use All Data Types</h1>
 
-echo "<h3>Negetive Numbers :</h3>" . PHP_EOL;
+    <h2>Calculate the area using Length and breath using Integer:</h2>
+    <?php
+    $length = 10;
+    $breath = 20;
+    $area = $length * $breath;
+    echo $area . "<br>";
+    ?>
+    <h2>Calculate Student Exact Marks Percentage with Double:</h2>
+    <?php
+    $english = 65;
+    $math = 95;
+    $total = $math + $english;
+    $totalMarks = 200;
+    $percentage = $total / $totalMarks;
+    echo "Percentage : " . $percentage . "<br>";
+    ?>
+    <h2>Check if the user has admin roles:</h2>
+    <?php
 
-$finalNumber = 15 - 16;
-echo $finalNumber . PHP_EOL;
+    $userName = "Faisal";
+    $hasRoles = null;
+    $adminRoles = "Admin";
+    $userRoles = "Admin";
 
-$fNumber = -11 + 8;
-echo $fNumber . PHP_EOL;
+    $hasRoles = ($adminRoles == $userRoles);
+    echo ($hasRoles) ? "$userName has Admin Roles." : "$userName has not assigned as admin." . "<br>";
 
-//--------------------------
-echo "<center><h1>Double Data Type</h1></center>" . PHP_EOL;
-//--------------------------
+    ?>
+    <h2>Define and Display a User Name using Strings:</h2>
+    <?php
+    $fName = "Faisal";
+    $lName = "Ahammed";
+    $fullName = $fName . " " . $lName;
+    echo $fullName;
+    ?>
+    <h2>Check if the variable is null or not:</h2>
+    <?php
 
-echo "<h3>Define and Print Decimal Variables :</h3>" . PHP_EOL;
-$totalNumber = 76.98;
-echo $totalNumber . PHP_EOL;
-echo "Negetive Decimals : <br>" . PHP_EOL;
-$negNumber = -87.84;
-echo $negNumber . PHP_EOL;
-$myRoundNumber = 6.45;
-echo round($myRoundNumber) . PHP_EOL;
-$price = 1.75;
-$bid = 1.26;
-echo (abs($price - $bid)< 0.97) ? "Accepted" : "Rejected" . PHP_EOL;
-//abs = absolute
+    $inputValue = "";
+    $hasValue = is_null($inputValue);
+    // Check
+    echo (!$hasValue) ? "Input has a Value" : "Input has no Value";
+    ?>
+</body>
 
-//--------------------------
-echo "<center><h1>Boolean Data Type</h1></center>" . PHP_EOL;
-//--------------------------
-
-
-echo "<h3>define and use the boolean variable :</h3>" . PHP_EOL;
-$flag1 = TRUE;
-$flag2 = FALSE;
-echo ($flag1) ? "True" : "False" . PHP_EOL;
-echo ($flag2) ? "True" : "False" . PHP_EOL;
-
-echo "<h3>Print Boolean Variables</h3>";
-echo "True: " . $flag1 . PHP_EOL; 
-echo "True: " . $flag2 . PHP_EOL;
-
-echo "<h3>Use String as Boolean Condition</h3>";
-
-$isLogin = true;
-$session = 1;
-echo ($isLogin && $session) ? "Login Successful" : "Login Failed";
-
-//--------------------------
-echo "<center><h1>String Data Type</h1></center>" . PHP_EOL;
-//--------------------------
- echo "<h3>Define String and Use String</h3>";
-$fName =  "Faisal";
-$lName = "Ahammed";
-$fullName = $fName ." ". $lName;
-echo "my first name is $fName and  last Name is $lName." .PHP_EOL;
-echo "my full name is $fullName" . PHP_EOL;
-
-$name4 = "This is a \"Special\" String";
-echo $name4 . PHP_EOL;
-
-$name4 = "\t \t This is a \"Special\" String";
-echo $name4 . PHP_EOL;
-
-echo strlen($name4) . PHP_EOL;
-
-//--------------------------
-echo "<center><h1>NULL Data Type</h1></center>" . PHP_EOL;
-//--------------------------
-
-unset($name);
-echo ( is_null($name) ) ? "Has Value" : "Empty" . PHP_EOL;
-
-unset($name);
-echo ( !is_null($name) ) ? "Has Value" : "Empty" . PHP_EOL;
-
-
-
-
-?>
+</html>
--- 05.Data Types/08.DataTypeExercise2.php
+++ 05.Data Types/08.DataTypeExercise2.php
@@ -1,5 +1,66 @@
-<?php
+<!doctype html>
+<html>
 
-// Print Student Details
+<head>
+    <title>
+        Data Types -Print Student Details
+    </title>
+</head>
 
-?>
+<body style="background-color:chartreuse">
+
+    <h1>Exercise 1: Print Student Details</h1>
+    <?php
+
+    $studentName = "Faisal";
+    $classSection = "5C";
+    $age = 25;
+    $gender = "Male";
+    $contact = "+8801701062266";
+
+    //Initialize the variables
+    $maths  = 0;
+    $science = 0;
+    $total = 0;
+    $totalMarks = 200;
+    $percentage = 0;
+    $passed = null;
+    ?>
+    <h2>Student Information:</h2>
+    <table border="1" style="padding:10px; border-radius:10px; background-color:antiquewhite; text-align:center; width: 600px;">
+        <tr>
+            <td>Name</td>
+            <td>Class</td>
+            <td>Age</td>
+            <td>Sex</td>
+            <td>Contact</td>
+        </tr>
+        <tr>
+            <td><?= $studentName ?></td>
+            <td><?= $classSection ?></td>
+            <td><?= $age ?></td>
+            <td><?= $gender ?></td>
+            <td><?= $contact ?></td>
+        </tr>
+    </table>
+    <h2>Exams Attended and Marks:</h2>
+    <?php
+    $maths = 87;
+    $science = 57;
+    $total = ($maths + $science);
+    $examAttended = ($total > 0) ? true : false;
+    ?>
+    Exam Attended : <?= ($examAttended) ? "Yes, Attend on exam !" : "No, Not atteded on exam !"?>
+    <h2>Final Marks:</h2>
+    Total Marks: <?= $totalMarks ?> 
+    <h2>Percentage:</h2>
+    <?php
+    $percentage = ($total / $totalMarks) * 100;
+    ?>
+    Percentage : <?php $percentage ?>
+    <h2>Result:</h2>
+    
+
+</body>
+
+</html>
