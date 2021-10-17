9e599040b79e76d5c7ed2b3795af45e44707a198
faisalahammed044274 <setu2014@gmail.com>
2021-10-17 23:48:02 +0600

    Data type -assignment

--- 05.Data Types/09.EmployerAssingment.php
+++ 05.Data Types/09.EmployerAssingment.php
@@ -1,10 +1,29 @@
-<?php
-// 1) Print the Employee Details
+<!-- // 1) Print the Employee Details
 // 2) Working Hours
 // 3) Employee Email ID and Address
 // 4) Check if Employer is Married or not
-// 5) Print the Employer Last Salary Drawn using Doubles
+// 5) Print the Employer Last Salary Drawn using Doubles -->
+
+<!doctype html>
+<html>
+<head>
+    <title>
+        Data Types
+    </title>
+</head>
+<body>
+
+<h1>Print the Employee Details</h1>
+
+<h2>Working Hours</h2>
+
+<h2>Employee Email ID and Address</h2>
+
+<h2>Check if Employer is Married or not</h2>
+
+<h2>Print the Employer Last Salary Drawn using Doubles</h2>
 
 
+</body>
+</html>
 
-?>
