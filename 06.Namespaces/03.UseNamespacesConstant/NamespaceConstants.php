5d6bbac4dabe62684c2e668c04fc759891eb55ea
faisalahammed044274 <setu2014@gmail.com>
2021-10-19 03:07:44 +0600

    Namespace

--- 06.Namespaces/03.UseNamespacesConstant/NamespaceConstants.php
+++ 06.Namespaces/03.UseNamespacesConstant/NamespaceConstants.php
@@ -6,6 +6,4 @@ namespace MyConstants;
 //Constants are allowed in the namespace.
 const FILE_NAME = __NAMESPACE__ . "NamespaceConstants.php" . PHP_EOL;
 
-
-
 ?>
--- 06.Namespaces/04.DefineNamespacesExercise1/04.DefineNamespacesExercise1.php
+++ 06.Namespaces/04.DefineNamespacesExercise1/04.DefineNamespacesExercise1.php
@@ -1,4 +1,11 @@
 <?php
 // 1) Define Constants File using Namespace and Use it in another php file
 // 2) Use Sub Namespace and Namespace Constants.
+
+include "MyNamespace.php";
+
+echo "Total Marks : " . StudentConstants\TOTAL_MARKS . PHP_EOL;
+echo "Passing Marks : " . StudentConstants\PASSING_MARKS . PHP_EOL;
+
+
 ?>
