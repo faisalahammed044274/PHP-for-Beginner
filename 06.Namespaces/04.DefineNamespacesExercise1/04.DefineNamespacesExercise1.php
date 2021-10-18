ae613808d972f1a71676e8b578929963c3aace7b
faisalahammed044274 <setu2014@gmail.com>
2021-10-19 03:19:05 +0600

    Namespace

--- 06.Namespaces/04.DefineNamespacesExercise1/04.DefineNamespacesExercise1.php
+++ 06.Namespaces/04.DefineNamespacesExercise1/04.DefineNamespacesExercise1.php
@@ -4,8 +4,8 @@
 
 include "MyNamespace.php";
 
-echo "Total Marks : " . StudentConstants\TOTAL_MARKS . PHP_EOL;
-echo "Passing Marks : " . StudentConstants\PASSING_MARKS . PHP_EOL;
+echo "Total Marks : " . StudentConstants\Student1\TOTAL_MARKS . PHP_EOL;
+echo "Passing Marks : " . StudentConstants\Student1\PASSING_MARKS . PHP_EOL;
 
-
+echo "FileName : " . \StudentConstants\Student1\FILE_NAME . PHP_EOL;
 ?>
--- 06.Namespaces/04.DefineNamespacesExercise1/myNameSpace.php
+++ 06.Namespaces/04.DefineNamespacesExercise1/myNameSpace.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace StudentConstants;
+namespace StudentConstants\Student1;
 
 const TOTAL_SUBJECTS = 7;
 const TOTAL_MARKS = 700;
 const PASSING_MARKS = 200;
-
+const FILE_NAME = __NAMESPACE__ . "\MyNamespace.php" . PHP_EOL;
 
 ?>
