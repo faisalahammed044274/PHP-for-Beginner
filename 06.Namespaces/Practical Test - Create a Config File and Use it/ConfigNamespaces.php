ae855e5e279cd5042168e8fcbd8c9c3337a728f5
faisalahammed044274 <setu2014@gmail.com>
2021-10-19 18:00:01 +0600

    Namespaces

--- 06.Namespaces/Practical Test - Create a Config File and Use it/ConfigNamespaces.php
+++ 06.Namespaces/Practical Test - Create a Config File and Use it/ConfigNamespaces.php
@@ -4,5 +4,8 @@
 // 2) Name the Constant file as ConfigNamespaces.php and Create some Constants.
 // 3) Use the Constants in the file UseNamespaces.php
 
+namespace MyConstants;
+
+const FILE_NAME = __NAMESPACE__ . "ConfigNamespaces.php" . PHP_EOL;
 
 ?>
--- 06.Namespaces/Practical Test - Create a Config File and Use it/UseNamespaces.php
+++ 06.Namespaces/Practical Test - Create a Config File and Use it/UseNamespaces.php
@@ -2,6 +2,9 @@
 // 1) Define a Config file with Constants.
 // 2) Name the Constant file as ConfigNamespaces.php and Create some Constants.
 // 3) Use the Constants in the file UseNamespaces.php
+//How to define namespace name in the php file?
 
+include "ConfigNamespaces.php";
 
+echo MyConstants\FILE_NAME;
 ?>
