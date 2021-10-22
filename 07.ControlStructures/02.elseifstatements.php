6fd8a92ddd1802a550fdbdd057c82e09952f869e
faisalahammed044274 <setu2014@gmail.com>
2021-10-22 19:56:59 +0600

    control structures

--- 07.ControlStructures/02.elseifstatements.php
+++ 07.ControlStructures/02.elseifstatements.php
@@ -1,1 +1,33 @@
-<!-- Check for Odd or Even Numbers and Print it. -->
+<?php
+//Check for Odd or Even Numbers and Print it.
+//Using elseif
+
+$input_number = 10;
+$result = $input_number % 2 ;
+if($result){
+    echo 'Odd Number' . PHP_EOL;
+}elseif(!$result){
+    echo 'Even Number' . PHP_EOL;
+}
+
+/*
+if(condition){
+    //Code to be executed if conditional is true
+} elseif(condition2){
+    //Code to be executed if the conditional is false and conditional 2 is true
+}else{
+    //Code to be executed if both conditional and condition2 are false
+}
+*/
+echo "<br>";
+//Check if the input_number is greater than 10
+
+if($input_number > 10){
+    echo "Number is greater than 10" . PHP_EOL;
+} elseif ($input_number == 10){
+    echo "Number is equal to 10" . PHP_EOL;
+}else{
+    echo "Number is less than 10" . PHP_EOL;
+}
+
+?>
--- 07.ControlStructures/04.forloop.php
+++ 07.ControlStructures/04.forloop.php
@@ -1,1 +1,41 @@
-<!-- Print Odd Numbers from 1 to 10. -->
+<?php
+
+//Print Odd Numbers from 1 to 10.
+
+/*
+for(initialization; condition; increment) {
+    //code to be executed
+}
+*/
+
+//Sample 1 - Recommend Way
+
+for ($value = 2; $value<=10;$value++){
+
+    $result = $value;
+    if ($result > 0) {
+        echo 'Odd Number' . $value . PHP_EOL;
+    }
+}
+echo '<br>';
+
+//Sample 2
+
+$value = 1;
+for (; $value <= 10; $value++) {
+
+    $result = $value % 2;
+    if ($result>0){
+        echo 'Odd Number' . $value . PHP_EOL;
+    }
+}
+
+// Sample 3
+
+$value = 1;
+for(; $value <= 10;)
+
+
+
+
+?>
