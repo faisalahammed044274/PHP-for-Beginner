6cdd75092536e3dc3f0edd56024caeac770bb9b3
faisalahammed044274 <setu2014@gmail.com>
2021-10-24 02:59:37 +0600

    using break - control structures

--- 07.ControlStructures/07.usingbreak.php
+++ 07.ControlStructures/07.usingbreak.php
@@ -26,14 +26,30 @@ $loop3 = 1 ;
 
 for ( ; $loop1 <= 10; $loop1++) {
 
-    while($loop2 <= 10) {
+    while( $loop2 <= 10 ) {
 
         do{
+            if($loop3 == 5){
+                break 1;
+            }
+            echo "Loop 3 : $loop3" . PHP_EOL .  "<br>";
+            echo '<hr>';
             $loop3++;
-        } while($loop3 <= 10);
+        } while ( $loop3 <= 10 );
+        if ( $loop2 == 9){
+            break 1;
+            
+        }
+        echo "Loop 2 : $loop2" . PHP_EOL .  "<br>";
+        echo '<hr>';
         $loop2++;
     }
+
+    if ($loop1 == 7){
+        break;
+    }
     echo "Loop 1 : $loop1" . PHP_EOL . "<br>";
+    echo '<hr>';
 }
 
 ?>
