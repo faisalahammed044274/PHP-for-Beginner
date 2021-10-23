299409abe703cabcbe95d29cd8fe41c051ea7065
faisalahammed044274 <setu2014@gmail.com>
2021-10-24 02:34:22 +0600

    Control Structures

--- 07.ControlStructures/07.usingbreak.php
+++ 07.ControlStructures/07.usingbreak.php
@@ -4,17 +4,36 @@
 //Break the infinite loop
 //Print 1 to 20 using infinite loop
 
-$value = 1;
+// $value = 1;
 
-do{
+// do{
 
-    echo $value . PHP_EOL;
+//     echo $value . PHP_EOL;
 
-    if ($value >= 2000){
-        break;
+//     if ($value >= 2000){
+//         break;
+//     }
+//     $value++;
+
+// }while(true);
+
+
+//Break Multiple loop
+
+$loop1 = 1 ;
+$loop2 = 1 ;
+$loop3 = 1 ;
+
+for ( ; $loop1 <= 10; $loop1++) {
+
+    while($loop2 <= 10) {
+
+        do{
+            $loop3++;
+        } while($loop3 <= 10);
+        $loop2++;
     }
-    $value++;
-
-}while(true);
+    echo "Loop 1 : $loop1" . PHP_EOL . "<br>";
+}
 
 ?>
