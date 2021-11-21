83fdd8a4933e645180f8f9774cddb805ec5d5b19
faisalahammed044274 <setu2014@gmail.com>
2021-11-21 12:39:23 +0600

    Array

---
+++ 09.Arrays/08.ArrayUnset.php
@@ -0,0 +1,20 @@
+<?php
+// Remove / Delete Elements from the Array Safely.
+
+$arr  = 5;
+print_r($arr);
+
+$arr1 = ["first" => 1, 2, 3, 4, "last" => 5];
+print_r($arr1);
+
+unset($arr1["last"]);
+print_r($arr1);
+
+$arr1["last"] = 5;
+print_r($arr1);
+
+$arr1[] = 6;
+print_r($arr1);
+
+
+?>
