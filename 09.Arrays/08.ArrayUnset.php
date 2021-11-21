71c68a65495e505e361be6dd1ba38ee979cdc853
faisalahammed044274 <setu2014@gmail.com>
2021-11-21 14:25:34 +0600

    Array Unset

--- 09.Arrays/08.ArrayUnset.php
+++ 09.Arrays/08.ArrayUnset.php
@@ -1,17 +1,27 @@
 <?php
 // Remove / Delete Elements from the Array Safely.
 
-$arr  = 5;
-print_r($arr);
 
-$arr1 = ["first" => 1, 2, 3, 4, "last" => 5];
-print_r($arr1);
+// $arr = [1, 2, 3, 4, 5];
+// print_r($arr);
+
+// print "<br>";
+
+// unset($arr[6]);
+// print_r($arr);
+// echo '<br>';
+// $arr[]  = 5;
+// print_r($arr);
+// echo '<br>';
+
+$arr1 = ["first" => 1, 2, 3, 4, "last" => 5,"final"=>67];
+print_r($arr1) . PHP_EOL;
 
 unset($arr1["last"]);
-print_r($arr1);
+print_r($arr1) . PHP_EOL;
 
 $arr1["last"] = 5;
-print_r($arr1);
+print_r($arr1) . PHP_EOL;
 
 $arr1[] = 6;
 print_r($arr1);
