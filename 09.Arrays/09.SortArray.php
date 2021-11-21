678f12f943045d0f92f0d9cb4a6c50fb011b1499
faisalahammed044274 <setu2014@gmail.com>
2021-11-21 21:56:44 +0600

    Sorted Array

--- 09.Arrays/09.SortArray.php
+++ 09.Arrays/09.SortArray.php
@@ -12,30 +12,34 @@
 //sort() and rsort()
 //Indexed Array
 //--------------------------
+// echo "sort and rsort" . PHP_EOL;
 
-$arr = [5, 3 ,6, 0, 2, 4];
-print_r($arr);
-sort($arr);
-rsort($arr);
-print_r($arr);
+// $arr = [5, 3 ,6, 0, 2, 4];
+// print_r($arr);
+// sort($arr); //Acending process
+// rsort($arr); //Decending process
+// print_r($arr);
 
 //--------------------------
 //asort() and arsort()
 //Associative array
 //--------------------------
+// echo "asort and arsort" . PHP_EOL;
 
 $arr1 = ["3"=>"Jhon" , "1" => "Smith", "2" => "Arya", "4" => "Doe"];
-print_r ($arr1);
-arsort($arr1);
-print_r ($arr1);
+// print_r ($arr1);
+//asort($arr1); //Acending sort by value
+// arsort($arr1); //Decending sort by value
+// print_r ($arr1);
 
-//ksort and krsort()
-//Associative array
+// //ksort and krsort()
+// //Associative array
+echo "ksort and krsort" . PHP_EOL;
 
-print_r($arr1);
-ksort($arr1);
-print_r($arr1);
-arsort($arr1);
+// print_r($arr1);
+ksort($arr1); // Acending sorted by key
+print_r($arr1) . PHP_EOL;
+krsort($arr1); // Decending sorted by key
 print_r($arr1);
 
 ?>
