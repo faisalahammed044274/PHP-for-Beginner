355c71a16d1cf9c54643cd3906777e3e42e3c8e0
faisalahammed044274 <setu2014@gmail.com>
2021-11-29 20:56:47 +0600

    Define & Call a Function

--- 10.Function/01.DefineFunctions.php
+++ 10.Function/01.DefineFunctions.php
@@ -1,5 +1,32 @@
 <?php
 // Define and Call a Function
 
+/* 
+    function function_name(param_1, ... , param_n)
+    {
+        statement_1;
+        statement_2;
+        ...
+        statement_m;
 
+        return return_value;
+    }
+*/
+
+//Define a function
+function printEvenNumbers($limit){
+    for ($index = 0; $index <= $limit; $index++){
+        if($index%2 == 0) {
+            echo "Even Number : $index" . PHP_EOL . "<br>";
+        }
+    }
+}
+//Calling the function
+printEvenNumbers(100);
+
+function add ($a, $b){
+    return $a + $b;
+}
+
+echo " Total Function add is :" . add(100,200) . PHP_EOL . "<br>";
 ?>
