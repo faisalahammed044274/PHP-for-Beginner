6f10273af637445e7a346e77534586e8f239747c
faisalahammed044274 <setu2014@gmail.com>
2021-11-30 22:51:13 +0600

    Prototypes- need revision

--- 10.Function/07.prototypes.php
+++ 10.Function/07.prototypes.php
@@ -7,3 +7,26 @@
 // Read : str_replace()
 
 // How to read functions from PHP Library.
+
+/*
+
+## We could rewrite the above function definition in a generic way:
+
+    function name    ( parameter type   parameter name ) : returned type
+
+## Many functions take on multiple parameters, such as in_array().
+Its prototype is as follows:
+
+    in_array ( mixed $needle, array $haystack , bool $strict = false ) : bool
+
+*/
+
+
+// str_replace(
+//     array|string $search,
+//     array|string $replace,
+//     string|array $subject,
+//     int &$count = null
+// ): string|array
+
+//Need Revision
