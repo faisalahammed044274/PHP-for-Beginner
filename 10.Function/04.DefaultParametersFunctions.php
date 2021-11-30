9fbe71e14925ef26312fc43051cf32f29928d7dd
faisalahammed044274 <setu2014@gmail.com>
2021-11-30 13:31:41 +0600

    Dafault Parameter Value

--- 10.Function/04.DefaultParametersFunctions.php
+++ 10.Function/04.DefaultParametersFunctions.php
@@ -1,34 +1,32 @@
 <?php
 // Assign Default and Optional Parameters to Functions
 
-declare (strict_types = 1);
+declare(strict_types=1);
 
- function printOddNumbers(int $limit, $skipNumber = 3){
-     for ($index = 0; $index <= $limit; $index++){
-         if($skipNumber == $index){
-             continue;
-         }
-         if ($index%2 != 0){
-             echo "Odd Number : $index " . PHP_EOL . "<br>";
-         }
-     }
- }
-
-    //Optional parameters should be last.
-    function printEvenNumbers($skipNumber, int $limit){ //$skipNumber = -1 //assign value is deprecated 
-        for($index = 0; $index <= $limit; $index++){
-            if($skipNumber == $index) {
-                continue;
-            }
-            if( $index%2 == 0){
-                echo "Even Number : $index" . PHP_EOL . "<br>";
-            }
+function printOddNumbers(int $limit, $skipNumber = -1) //Write your default parameter in the last.
+{
+    for ($index = 0; $index <= $limit; $index++) {
+        if ($skipNumber == $index) {
+            continue;
+        }
+        if ($index % 2 != 0) {
+            echo "Odd Number : $index " . PHP_EOL . "<br>";
         }
     }
-    printOddNumbers(15);
-    echo "<br>";
-   printEvenNumbers( 12, 15);
+}
 
-   //Start
-
-?>
+//Optional parameters should be last.
+function printEvenNumbers(int $limit, $skipNumber = -1) //Write your default parameter in the last.
+{ //$skipNumber = -1 //assign value is deprecated 
+    for ($index = 0; $index <= $limit; $index++) {
+        if ($skipNumber == $index) {
+            continue;
+        }
+        if ($index % 2 == 0) {
+            echo "Even Number : $index" . PHP_EOL . "<br>";
+        }
+    }
+}
+printOddNumbers(15, 3);
+echo "<br>";
+printEvenNumbers(20, 6);
