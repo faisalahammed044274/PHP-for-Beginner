e5dab37b2996678e55a2fe9a279721c82d1f0e42
faisalahammed044274 <setu2014@gmail.com>
2021-12-04 05:43:06 +0600

    Raise catch exception

--- 11.Handling Exceptions/01.RaiseCatchException.php
+++ 11.Handling Exceptions/01.RaiseCatchException.php
@@ -1,4 +1,28 @@
 <?php
 //Raise and Catch a Divide by Zero Error.
 
+
+function divide($x, $y) : int{
+    if ($y <= 0){
+        throw new Exception("Divide by Zero Exception 
+        might happen");
+    }
+
+    $result = $x / $y;
+    return $result;
+}
+
+try{
+    //This is a error in this block
+    $result = divide(1,0);
+
+
+}catch(Exception $e){
+    //Jump to this block if try block has an error
+    echo "Caught Exception" . $e->getMessage() . PHP_EOL ;
+
+}
+
+
+
 ?>
