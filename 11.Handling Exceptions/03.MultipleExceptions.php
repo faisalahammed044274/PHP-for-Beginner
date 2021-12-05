4f4d9d12290497bde0ada44d48d78aa95af93a57
faisalahammed044274 <setu2014@gmail.com>
2021-12-05 15:53:43 +0600

    Multiple Exception

--- 11.Handling Exceptions/03.MultipleExceptions.php
+++ 11.Handling Exceptions/03.MultipleExceptions.php
@@ -1,3 +1,29 @@
 <?php
 // Catch Multiple Exceptions.
-?>
+
+function custom_exception_handler($exception)
+{
+    echo 'Caught Exception : ' . $exception->getMessage() . PHP_EOL;
+}
+
+function divide($x, $y): int
+{
+
+    try {
+
+        if ($y <= 0) {
+
+            throw new Exception("Divide by Zero Exception might happen");
+        }
+
+        $result = $x / $y;
+    } catch (Exception $e) {
+        throw new Exception("Divide method has input param is less than zero" . $e->getMessage());
+    }
+
+}
+
+
+
+set_exception_handler('Custom_exception_handler');
+divide(1, 0);
