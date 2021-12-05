a8e8f40b1685f9958600f5ffc8dcac9720dde0ce
faisalahammed044274 <setu2014@gmail.com>
2021-12-06 04:07:17 +0600

    Exception

--- 11.Handling Exceptions/04.FinallyException.php
+++ 11.Handling Exceptions/04.FinallyException.php
@@ -1,3 +1,18 @@
 <?php
 // Raise the Exception and execute finally block
+
+    function custom_exception_handler($exception){
+        echo 'Caught Exception : ' . $exception->getMessage() . PHP_EOL;
+    }
+
+    set_exception_handler('custom_exception_handler');
+
+    try{
+        throw new Exception("Exception is raised !");
+    }finally{
+        //This block always get executed with / without exception
+        echo "This line is executed before exception" . PHP_EOL . "<br>";
+    }
+
+
 ?>
