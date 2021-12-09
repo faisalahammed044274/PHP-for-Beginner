3c731a6e6d4a6411f0c45b4c0f4811bdffe00c96
faisalahammed044274 <setu2014@gmail.com>
2021-12-10 01:16:25 +0600

    Files

--- 12.Files/08.php
+++ 12.Files/08.php
@@ -5,4 +5,28 @@
 // 3) Read the CSV File and Store the Students details in Array
 // 4) Iterate the Array and Print the Students Table in HTML Format.
 
+$fileName = "student.csv";
+$csvFile = file($fileName);
+foreach ($csvFile as $line) {
+    $data[] = str_getcsv($line);
+    print_r($line) . PHP_EOL;
+}
 ?>
+<html>
+<body>
+    <table border="1px" height="50px" width= "350px" style="background-color:bisque; color:darkgreen; text-align:center;">
+        <tr>
+        <th>Student Data</th>
+        </tr>
+        <tr>
+        <td><?php
+        foreach($csvFile as $line){
+            $data[] = str_getcsv($line);
+            echo $line . "<br>";
+        }
+        ?></td>
+        </tr>
+    </table>
+</body>
+
+</html>
