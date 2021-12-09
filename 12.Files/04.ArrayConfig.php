fa5825f3002de9758fab8e097152a474e25f9abe
faisalahammed044274 <setu2014@gmail.com>
2021-12-09 23:17:04 +0600

    Files

--- 12.Files/04.ArrayConfig.php
+++ 12.Files/04.ArrayConfig.php
@@ -2,4 +2,14 @@
 
 //Read an Array from the File
 
+
+$settings = parse_ini_file("test.ini");
+print_r($settings);
+
+foreach($settings as $key => $val){
+    echo $key . '=>' . $val . PHP_EOL;
+}
+
+echo $settings['white'] . PHP_EOL;
+echo $settings['font1'] . PHP_EOL;
 ?>
