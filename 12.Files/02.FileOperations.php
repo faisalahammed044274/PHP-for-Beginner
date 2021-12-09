6b07aaff5926993a1f2e524306eac9a2927bed9a
faisalahammed044274 <setu2014@gmail.com>
2021-12-09 07:06:34 +0600

    Files

--- 12.Files/02.FileOperations.php
+++ 12.Files/02.FileOperations.php
@@ -1,3 +1,33 @@
 <?php
-// How to Work with File Operations
+// How to Work with File Operations (Copy, Rename and Delete Files )
+
+//Check if file exists
+$fileName = "MyFolder.txt";
+
+if (file_exists($fileName)){
+    echo "File Exists." . PHP_EOL;
+
+        //Check if it is a Directory
+        if(is_dir($fileName)){
+            die("It is a Directory not File.");
+        }
+
+        //Copy File
+        copy($fileName, "CopyFile.txt");
+        copy("CopyFile.txt","CopyFile1.txt");
+
+        //Rename the Files
+        rename("CopyFile1.txt","RenamedFile.txt");
+        copy("RenamedFile.txt","RenamedFileDeleted.txt");
+
+        //Deleted Files
+        unlink("RenamedFileDeleted.txt");
+
+}else{
+    echo "File/Directory Doesnot exists" . PHP_EOL;
+    die("No Such Files");
+}
+
+
+
 ?>
--- 12.Files/03.ReadWriteFiles.php
+++ 12.Files/03.ReadWriteFiles.php
@@ -1,3 +1,48 @@
 <?php
 //Learn to Read and Write Files
+
+
+//Open Files
+//Read Files
+//Close the Files
+
+//File Modes
+//r - read mode
+//w - write
+//a - append
+
+//======================
+//Method 1
+//======================
+
+//File Name
+$fileName = "readwrite.txt";
+
+//Read the file
+$content = file_get_contents($fileName);
+
+//Print the Content of the file
+echo $content . PHP_EOL; 
+
+//Open a File
+$fileHandler = fopen($fileName, "r");
+$fileSize = filesize($fileName);
+
+//Read the File Content
+$content = fread($fileHandler, $fileSize);
+echo $content . PHP_EOL;
+
+//Close the file
+fclose($fileHandler);
+
+//Write Operations - Method 1
+$fileHandler = fopen($fileName, "w") or die("Unable to Write the file");
+
+fwrite($fileHandler, "This is a modified content");
+fclose($fileHandler);
+
+//======================
+//Method 2
+//======================
+
 ?>
